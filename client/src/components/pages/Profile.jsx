aa11c57818bad0de76ff234369edb46897703066
Logan <25yeel@gmail.com>
2026-01-28 13:00:21 -0500

    add-ons

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -33,7 +33,7 @@ const Profile = (props) => {
                         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
                       )} */}
                 {/* Display number of times solved mazes:  */}
-                <h3>Achievements: {achievements}</h3>
+                <h3>Mazes solved: {achievements}</h3>
             </div>
         </>
         // </body>
